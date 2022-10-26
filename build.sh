7ba9de10007be97894adad3dcc281e0351da4729
Augusto Zanellato <augusto.zanellato@gmail.com>
2022-10-27 00:13:13 +0200

    Primo verbale + roba devops

---
+++ build.sh
@@ -0,0 +1,25 @@
+shopt -s expand_aliases
+
+if [ "$CI" = true ]; then
+    alias pandoc='docker run -i --rm -v "$(pwd)/:/work" -w /work dalibo/pandocker:latest-buster-full'
+fi
+
+pandoc --version
+mkdir -p _site
+# Compile markdown
+for S in `find . -name "*.md" -not -path "./template*"`
+do
+  DESTDIR=$(echo out/`dirname $S`)
+  DESTNAME=$(echo $DESTDIR/`basename $S ".md"`.pdf)
+  mkdir -p $DESTDIR
+  pandoc $S -o $DESTNAME --from markdown --template template/eisvogel.tex --listings
+done
+
+# Copy everything else
+for S in `find . -type f -not -path "./template*" -not -path "./.git*" -not -path "./.vscode*" -not -path "./build.sh" -not -name "*.md" -not -path "./out*" -not -path "./assets*"`
+do
+  DESTDIR=$(echo out/`dirname $S`)
+  DESTNAME=$(echo $DESTDIR/`basename $S`)
+  mkdir -p $DESTDIR
+  cp $S $DESTNAME
+done
