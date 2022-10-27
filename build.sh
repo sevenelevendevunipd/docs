f4a2c91a73411f3eafe02dd03bd4971959e0381e
Augusto Zanellato <augusto.zanellato@gmail.com>
2022-10-27 09:40:49 +0200

    fix build path

--- build.sh
+++ build.sh
@@ -8,16 +8,16 @@ fi
 # Compile markdown
 for S in `find . -name "*.md" -not -path "./template*"`
 do
-  DESTDIR=$(echo out/`dirname $S`)
+  DESTDIR=$(echo docs/`dirname $S`)
   DESTNAME=$(echo $DESTDIR/`basename $S ".md"`.pdf)
   mkdir -p $DESTDIR
   pandoc $S -o $DESTNAME --from markdown --template template/eisvogel.tex --listings
 done
 
 # Copy everything else
-for S in `find . -type f -not -path "./template*" -not -path "./.git*" -not -path "./.vscode*" -not -path "./build.sh" -not -name "*.md" -not -path "./out*" -not -path "./assets*"`
+for S in `find . -type f -not -path "./template*" -not -path "./.git*" -not -path "./.vscode*" -not -path "./build.sh" -not -name "*.md" -not -path "./docs*" -not -path "./assets*"`
 do
-  DESTDIR=$(echo out/`dirname $S`)
+  DESTDIR=$(echo docs/`dirname $S`)
   DESTNAME=$(echo $DESTDIR/`basename $S`)
   mkdir -p $DESTDIR
   cp $S $DESTNAME
